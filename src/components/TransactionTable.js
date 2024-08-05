f3176f5837133df760a5788ae95d06066f49e82b
code-iddih <idinyamwaya@gmail.com>
2024-08-05 23:45:27 +0300

    Adds Sort By Category

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import TransactionRow from './TransactionRow';
 
-function TransactionTable({ transactions, searchTerm, sortOrder, setSortOrder, deleteTransaction, sortBy, setSortBy }) {
-  const filteredTransactions = transactions.filter((transaction) =>
-    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+function TransactionTable({
+  transactions,
+  searchTerm,
+  sortOrder,
+  sortBy,
+  setSortOrder,
+  setSortBy,
+  deleteTransaction,
+  selectedCategory,
+}) {
+  const filteredTransactions = transactions
+    .filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .filter((transaction) => {
+      return selectedCategory ? transaction.category === selectedCategory : true;
+    });
 
   const sortedTransactions = filteredTransactions.sort((a, b) => {
     if (sortBy === 'description') {
@@ -16,7 +29,7 @@ function TransactionTable({ transactions, searchTerm, sortOrder, setSortOrder, d
         ? a.category.localeCompare(b.category)
         : b.category.localeCompare(a.category);
     }
-    return 0; // No sorting
+    return 0;
   });
 
   return (
